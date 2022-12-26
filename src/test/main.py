9e5947215c8af9cb185afd1c1d049a3570f2f6c9
shwaayyy <shwaayyy@gmail.com>
2022-12-26 18:07:42 +0700

    fix: cant play multiple test

--- src/test/main.py
+++ src/test/main.py
@@ -20,13 +20,14 @@ class TestWebsite:
 
         self.driver.maximize_window()
         self.driver.implicitly_wait(15)
+        self.driver.delete_all_cookies()
 
         self.driver.get("https://app.digisign.id/")
 
         yield
 
-        self.driver.close()
-        self.driver.quit()
+        # self.driver.close()
+        # self.driver.quit()
 
     def reg_and_log_object(self, element):
         match element:
@@ -54,5 +55,32 @@ class TestWebsite:
 
     def document_object(self, element):
         match element:
+            case "nav_inbox":
+                return self.driver.find_element(By.XPATH, "//li[.//i[@class='ti-write']]")
+            case "kotak_masuk":
+                return self.driver.find_element(
+                    By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[3]/div[1]/nav/ul/li[2]/ul/li[3]/a")
             case "need_sign":
                 return self.driver.find_element(By.XPATH, "//a[contains(@href, 'needsign')]")
+            case "check_all_sign":
+                return self.driver.find_element(By.XPATH, "//label[@for='idbox']")
+            case "sign_all_btn":
+                return self.driver.find_element(By.XPATH, "//button[@id='signnow']")
+            case "proses_btn":
+                return self.driver.find_element(By.XPATH, "//button[contains(@class, 'swal2-confirm')]")
+            case "check_doc1":
+                return self.driver.find_element(By.XPATH, "//label[@for='checkbox1']")
+            case "check_doc2":
+                return self.driver.find_element(By.XPATH, "//label[@for='checkbox2']")
+            case "check_doc3":
+                return self.driver.find_element(By.XPATH, "//label[@for='checkbox3']")
+            case "otp_input_number":
+                return self.driver.find_element(By.XPATH, "//*[@id='otp']")
+            case "otp_email":
+                return self.driver.find_element(By.XPATH, "//*[@id='otemail']")
+            case "proses_doc_btn_submit":
+                return self.driver.find_element(By.XPATH, "//*[@id='ps12']")
+            case "yakin_btn":
+                return self.driver.find_element(By.XPATH, "//button[contains(@class, 'swal2-confirm')]")
+            case "btn_selesai":
+                return self.driver.find_element(By.XPATH, "//a[@class='btn btn-info']")
--- src/test/test_reg.py
+++ src/test/test_reg.py
@@ -8,5 +8,6 @@ class TestReg(main.TestWebsite):
 
     def test_reg_nik(self):
         self.reg_and_log_object("link_reg").click()
-        self.reg_and_log_object("uname").send_keys("8928839489849203")
+        main.delay(0.5)
+        self.reg_and_log_object("nik_input").send_keys("8928839489849203")
         main.delay(3)
