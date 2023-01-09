b5376f0e6cb9198260ca9cc62f795627585f9e45
shwaayyy <shwaayyy@gmail.com>
2023-01-09 09:57:18 +0700

    fix: replace position of positive test

--- src/test/test_send_doc.py
+++ src/test/test_send_doc.py
@@ -4,42 +4,6 @@ from main import delay, DocObject, FormObject
 
 
 class TestSendDocument(FormObject, DocObject):
-    def test_send_document_full(self):
-        self.username().send_keys("wahyuhidy" + self.keys.ENTER)
-        delay(2)
-        self.password().send_keys("Kijang321!" + self.keys.ENTER)
-        delay(4)
-        self.choose_account().click()
-
-        delay(4)
-
-        self.doc_file().send_keys("C:\\Users\\dignitas\\Downloads\\company_image_20221101065745 (1) (1).pdf")
-        delay(2)
-        self.doc_submit().click()
-
-        delay(2)
-
-        self.check_seal_doc().click()
-        self.name_first_receiver().send_keys("digisign")
-        self.email_first_receiver().send_keys("ditest10@tandatanganku.com")
-        self.btn_detail_doc().click()
-        delay(2)
-        self.btn_add_sign().click()
-
-        delay(4)
-
-        self.actions.drag_and_drop_by_offset(self.sign_zone_1(), 10, 150).perform()
-        self.actions.drag_and_drop_by_offset(self.resizing_zone_1(), 30, 20).perform()
-
-        delay(5)
-
-        self.lock_sign_1().click()
-        self.btn_set_email().click()
-        self.btn_send_doc().click()
-        self.btn_process_send_doc().click()
-
-        delay(10)
-
     def test_send_document(self):
         self.username().send_keys("ditest10@tandatanganku.com" + self.keys.ENTER)
         delay(2)
@@ -170,3 +134,39 @@ class TestSendDocument(FormObject, DocObject):
         self.confirm_after_send_doc().click()
 
         delay(5)
+
+    def test_send_document_full(self):
+        self.username().send_keys("wahyuhidy" + self.keys.ENTER)
+        delay(2)
+        self.password().send_keys("Kijang321!" + self.keys.ENTER)
+        delay(4)
+        self.choose_account().click()
+
+        delay(4)
+
+        self.doc_file().send_keys("C:\\Users\\dignitas\\Downloads\\company_image_20221101065745 (1) (1).pdf")
+        delay(2)
+        self.doc_submit().click()
+
+        delay(2)
+
+        self.check_seal_doc().click()
+        self.name_first_receiver().send_keys("digisign")
+        self.email_first_receiver().send_keys("ditest10@tandatanganku.com")
+        self.btn_detail_doc().click()
+        delay(2)
+        self.btn_add_sign().click()
+
+        delay(4)
+
+        self.actions.drag_and_drop_by_offset(self.sign_zone_1(), 10, 150).perform()
+        self.actions.drag_and_drop_by_offset(self.resizing_zone_1(), 30, 20).perform()
+
+        delay(5)
+
+        self.lock_sign_1().click()
+        self.btn_set_email().click()
+        self.btn_send_doc().click()
+        self.btn_process_send_doc().click()
+
+        delay(10)
